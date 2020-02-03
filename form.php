3062015c542eacbea26e137e8af5ea0f7f529bb0
Philip Smith <philipm.smith@icloud.com>
2020-02-03 11:42:43 -0600

    Update form.php

--- form.php
+++ form.php
@@ -1,15 +1,17 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $mailFrom = $_POST['email'];
+        $message = $_POST['message'];
 
-    $mailTo = "philipm.smith@icloud.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name.".\n\n".$message;
+        $mailTo = "philipm.smith@icloud.com";
+        $headers = "From: ".$mailFrom;
+        $txt = "You have received an email from ".$name.".\n\n".$message;
 
-    
-    mail($mailTo, $headers, $txt );
-    header("Location: index.html?mailsend");
-}
+        
+        mail($mailTo, $headers, $txt );
+        header("Location: index.html?mailsend");
+    }
+
+?>
