ff8dd0e1b41cc385c42b20e39f85f6af43f74b3f
Philip Smith <philipm.smith@icloud.com>
2020-02-03 12:28:44 -0600

    Netlify Form

--- form.php
+++ form.php
@@ -1,31 +1,4 @@
-<?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
 
-
-$email_from = 'philipm.smith@icloud.com.com';
-
-$email_subject = "New Contact Form Submission";
-
-$email_body = "From: $name.\n".
-"Email: $visitor_email.\n".
-"Message: $message.\n";
-
-
-$to = "philipm.smith@icloud.com";
-
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: index.html");
-
-
-
-?>
 
 
 
